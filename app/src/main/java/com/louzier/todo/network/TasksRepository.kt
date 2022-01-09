8efcccf6f945a190a8f53dd330bef4c87e3420e1
Gabriel Meziere <gabriel.meziere7@orange.fr>
2022-01-09 16:47:41 +0100

    création nouvelle branche test

--- app/src/main/java/com/louzier/todo/network/TasksRepository.kt
+++ app/src/main/java/com/louzier/todo/network/TasksRepository.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
+
 class TasksRepository {
     private val tasksWebService = Api.tasksWebService
 
